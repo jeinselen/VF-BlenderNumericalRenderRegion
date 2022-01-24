14bc884059029ee04071b50b116aa871ff239b6a
John Einselen VF <jeinselen@vectorform.com>
2022-01-24 13:40:40 -0500

    2.90 and 3.0 series
    Includes versions for both 2.90 and 3.0 Blender series

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# VF-BlenderNumericalRenderRegion
+# VF Numerical Render Region
+
 Adds numerical input fields for more precise control over Blender's Render Region.
+
+![screenshot of the Blender Render Settings interface with the add-on installed](images/screenshot.png)
+
+## Installation and Usage
+
+- Download the appropriate .py add-on file (available for both 2.90 and 3.0 series)
+- Install and enable in the Blender Preferences Add-on tab
+- Whenever Render Region is enabled, four numerical fields will appear at the bottom of the Output Settings Format panel (Dimensions panel in 2.90)
+- `Bottom`, `Top`, `Left`, and `Right` are all 0.0-1.0 numerical values relative to the total width and height of the render (leaving `Bottom` at 0.0 and changing `Top` to 0.5, for example, will render only the lower half of the image).
+
+There are no further settings or preferences, this is just a super simple add-on to make adjusting the render region more precise and controllable by revealing the native numerical settings. Perhaps helpful for rendering small patches in an animation or segmenting an exceptionally high resolution render.
+
+Note that some render engines, such as Radeon ProRender, may not implement render regions reliably and will result in multi-pixel shifts that don't align as expected (this error may be worse/better depending on how the resolution divides by the region percentage).
---
+++ VF_numericalRenderRegion-2.90.py
@@ -0,0 +1,61 @@
+bl_info = {
+	"name": "VF Numerical Render Region",
+	"author": "John Einselen - Vectorform LLC",
+	"version": (0, 1),
+	"blender": (2, 90, 0),
+	"location": "Rendertab > Output Panel > Subpanel",
+	"description": "Adds Blender's native numerical render region settings to the render output tab",
+	"warning": "inexperienced developer, use at your own risk",
+	"wiki_url": "",
+	"tracker_url": "",
+	"category": "Render"}
+
+# Based on the following resources:
+# https://b3d.interplanety.org/en/setting-render-border-coordinates/
+
+import bpy
+
+###########################################################################
+# Project settings and UI rendering classes
+
+class RENDER_PT_numerical_render_region(bpy.types.Panel):
+	bl_space_type = 'PROPERTIES'
+	bl_region_type = 'WINDOW'
+	bl_context = "render"
+	bl_label = "Render Region Values"
+	bl_parent_id = "RENDER_PT_dimensions"
+	# bl_options = {'DEFAULT_CLOSED'}
+	bl_options = {'HIDE_HEADER'}
+
+	# def draw_header(self, context):
+		# self.layout.prop(bpy.context.scene.render, 'use_border', text='')
+
+	def draw(self, context):
+		if bpy.context.scene.render.use_border:
+			layout = self.layout
+			layout.use_property_decorate = False  # No animation
+			layout.use_property_split = True
+
+			# row = layout.row(align=True, heading='')
+			# row.prop(bpy.context.scene.render, 'border_min_y', text='Bottom | Top | Left | Right')
+			# row.prop(bpy.context.scene.render, 'border_max_y', text='')
+			# row.prop(bpy.context.scene.render, 'border_min_x', text='')
+			# row.prop(bpy.context.scene.render, 'border_max_x', text='')
+
+			col = layout.column(align=True)
+			col.prop(bpy.context.scene.render, 'border_min_y', text='Render Region Bottom')
+			col.prop(bpy.context.scene.render, 'border_max_y', text='Top')
+			col.prop(bpy.context.scene.render, 'border_min_x', text='Left')
+			col.prop(bpy.context.scene.render, 'border_max_x', text='Right')
+
+###########################################################################
+# Addon registration functions
+
+def register():
+	bpy.utils.register_class(RENDER_PT_numerical_render_region)
+
+def unregister():
+	bpy.utils.unregister_class(RENDER_PT_numerical_render_region)
+
+if __name__ == "__main__":
+	register()
